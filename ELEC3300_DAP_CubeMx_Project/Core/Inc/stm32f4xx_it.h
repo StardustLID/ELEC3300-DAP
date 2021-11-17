2922a364f24b9fef7910b312dbbe8341ff1a5142
StardustLID <60168779+StardustLID@users.noreply.github.com>
2021-11-17 15:32:29 +0800

    f4 menu files

--- ELEC3300_DAP_CubeMx_Project/Core/Inc/stm32f4xx_it.h
+++ ELEC3300_DAP_CubeMx_Project/Core/Inc/stm32f4xx_it.h
@@ -57,6 +57,7 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void DMA1_Stream5_IRQHandler(void);
+void TIM6_DAC_IRQHandler(void);
 void DMA2_Stream3_IRQHandler(void);
 void DMA2_Stream6_IRQHandler(void);
 void OTG_HS_IRQHandler(void);
