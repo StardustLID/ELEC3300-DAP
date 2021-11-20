35c7dc84650cc098ba9b956f6ba7b7e1ae8eaea1
StardustLID <60168779+StardustLID@users.noreply.github.com>
2021-11-20 16:49:11 +0800

    delete F1 projects

--- SDIO/MDK-ARM/SDIO/dequant.d
+++
@@ -1,6 +0,0 @@
-sdio\dequant.o: ..\Mp3\real\dequant.c
-sdio\dequant.o: ..\Mp3\real\coder.h
-sdio\dequant.o: ../MP3/pub/mp3common.h
-sdio\dequant.o: ../MP3/pub/mp3dec.h
-sdio\dequant.o: ../MP3/pub/statname.h
-sdio\dequant.o: ..\Mp3\real\assembly.h
--- SDIO/MDK-ARM/SDIO/mp3tabs.d
+++
@@ -1,4 +0,0 @@
-sdio\mp3tabs.o: ..\Mp3\mp3tabs.c
-sdio\mp3tabs.o: ../MP3/pub/mp3common.h
-sdio\mp3tabs.o: ../MP3/pub/mp3dec.h
-sdio\mp3tabs.o: ../MP3/pub/statname.h
